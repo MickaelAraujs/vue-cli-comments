c98b78925b34ed5408ed46aa01a7d683c482efbc
Mickael Araujo <araujomickael18@gmail.com>
2020-09-04 01:04:32 -0300

    comments app using cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="title">Comentários</h1>
+
+    <CommentsForm v-on:form-data="handleCommentInsertion" />
+
+    <hr>
+
+    <section class="section">
+      <p v-if="comments.length <=0">Sem comentários...</p>
+        <div
+            class="card"
+            v-for="(commentData, index) in allComments"
+            v-bind:key="index"
+        >
+          <div class="card-content">
+            <div class="content">
+              <strong>Autor:</strong>
+              <span>{{commentData.user}}</span>
+            </div>
+  
+            <div class="content">
+              <strong>Comentário</strong>
+              <span>{{commentData.message}}</span>
+            </div>
+          </div>
+
+          <button class="button is-danger is-small" v-on:click="handleCommentDelete(index)">
+            Excluir
+          </button>
+
+          <hr />
+        </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import CommentsForm from './components/CommentsForm.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+
+    components: {
+      CommentsForm,
+    },
+
+    data() {
+      let storageComments = this.getCommentsFromStorage();
+
+      if (!storageComments) {
+        storageComments = [];
+      }
+
+      return {
+        comments: storageComments,
+      };
+    },
+
+    methods: {
+      handleCommentInsertion(comment) {
+        this.comments.unshift(comment);
+      },
+
+      handleCommentDelete(commentIndex) {
+        this.comments.splice(commentIndex, 1);
+      },
+
+      getCommentsFromStorage() {
+        const comments = JSON.parse(localStorage.getItem('saved-comments'));
+
+        return comments;
+      },
+
+      setCommentsToStorage() {
+        localStorage.setItem('saved-comments', JSON.stringify(this.comments));
+      }
+    },
+
+    computed: {
+      allComments() {
+        return this.comments.map(comment => ({
+          ...comment,
+          user: comment.user.trim() === '' ? 'Anônimo' : comment.user
+        }));
+      }
+    },
+
+    watch: {
+      comments() {
+        this.setCommentsToStorage();
+      }
+    }
+
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CommentsForm.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="section">
+        <form class="form" v-on:submit.prevent="handleFormSubmit">
+            <div class="field">
+                <label class="label" for="name">Usuário</label>
+                <input
+                    class="input"
+                    type="text"
+                    placeholder="Seu nome"
+                    name="name"
+                    v-model="user"
+                >
+            </div>
+
+            <div class="field">
+                <label class="label" for="name">Comentário</label>
+                <input
+                    class="input"
+                    type="text"
+                    placeholder="Digite um comentário"
+                    name="name"
+                    v-model="message"
+                >
+            </div>
+
+            <div class="hero">
+                <button
+                    class="button is-success"
+                    type="submit"
+                >
+                    Comentar
+                </button>
+            </div>
+        </form>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'CommentsForm',
+
+        data() {
+            return {
+                user: '',
+                message: ''
+            };
+        },
+
+        methods: {
+            handleFormSubmit() {
+                if (this.message.trim() === '') return;
+
+                this.$emit('form-data', {
+                    user: this.user,
+                    message: this.message
+                });
+
+                this.user = '';
+                this.message = '';
+            }   
+        }
+    }
+</script>
